43dde35481fb7612ece9ed67dcdfabe79352c9d4
joseluisgs <joseluisgonsan@gmail.com>
2020-11-01 10:03:41 +0100

    Path personalizado para BBBDD

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const jsonServer = require('json-server')
 const server = jsonServer.create()
-const router = jsonServer.router('db.json')
+
+const path = require('path')
+const router = jsonServer.router(path.join(__dirname, '/bd/db.json'))
+
+// const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
@@ -9,5 +13,5 @@ server.use(router)
 const port = process.env.PORT || 6969;
 server.listen(port, () => {
   console.log(`⚑ Servidor JSON funcionando ✓ -> http://localhost:${port}`);
-  console.log(`⚑ Fake API REST por joseluisgs ✓ -> http://github.com/joseluis`);
+  console.log(`⚑ Fake API REST por joseluisgs ✓ -> http://github.com/joseluisgs`);
 })
