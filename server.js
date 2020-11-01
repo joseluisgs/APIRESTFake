ef77531f6dfd3da4867d44f41cdde006a48d5bbc
joseluisgs <joseluisgonsan@gmail.com>
2020-11-01 10:28:00 +0100

    ajustes BD

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
-const jsonServer = require('json-server')
-const server = jsonServer.create()
+const jsonServer = require('json-server');
+const server = jsonServer.create();
 
 // si quieres cambiar el path de tu bd o nombre de fichero json. 
 // Lo tengo puesto en la raiz para que sea accesible desde 
 // https://my-json-server.typicode.com/joseluisgs/APIRESTFake
-const path = require('path')
-// const router = jsonServer.router(path.join(__dirname, '/bd/db.json'))
-const router = jsonServer.router('db.json')
+// es una BD Limitada, la completa la tienes en el el directorio BD y es la que usa este servidir
+const path = require('path');
+const router = jsonServer.router(path.join(__dirname, '/bd/db.json'));
+//const router = jsonServer.router('db.json') // BD Limitada
 
-const middlewares = jsonServer.defaults()
+const middlewares = jsonServer.defaults();
 
-server.use(middlewares)
-server.use(router)
+server.use(middlewares);
+server.use(router);
 
 const port = process.env.PORT || 6969;
 server.listen(port, () => {
