db7a1bcf72aa2aa32b99f39909a0fd3793089b2f
joseluisgs <joseluisgonsan@gmail.com>
2020-11-04 12:32:38 +0100

    POST, PUT

--- tests/server.test.js
+++ tests/server.test.js
@@ -5,6 +5,9 @@
 import request from 'supertest';
 import server from '../src/server';
 
+// Variable o id del usuario para trabajar
+let userTestID;
+
 describe('Sample Test', () => {
   it('should test that true === true', () => {
     expect(true).toBe(true);
@@ -37,15 +40,62 @@ describe('Batería de tests de Users', () => {
       expect(res.statusCode).toEqual(200);
     });
     test('GET by ID: Obtiene el usuario con un ID', async () => {
-      const userId = 1;
-      const res = await request(servicio).get(`/users/${userId}`);
+      const userID = 1;
+      const res = await request(servicio).get(`/users/${userID}`);
       expect(res.statusCode).toEqual(200);
-      expect(res.body.id).toEqual(userId);
+      // Es el id que queremos
+      expect(res.body.id).toEqual(userID);
+      // Chequeamos propiedades
+      expect(res.body).toHaveProperty('name');
+      expect(res.body).toHaveProperty('username');
     });
     test('GET by ID: NO Obtiene el usuario con un ID', async () => {
-      const userId = "pepe";
-      const res = await request(servicio).get(`/users/${userId}`);
+      const userID = 'pepe';
+      const res = await request(servicio).get(`/users/${userID}`);
       expect(res.statusCode).toEqual(404);
+    });
+  });
+  describe('POST: Tests de insercción de usuarios ', () => {
+    test('POST: Inserta un usuario ', async () => {
+      const newUser = {
+        name: 'Usuario Inserccion',
+        username: 'usertest',
+        email: 'insert@insert.test',
+        avatar: 'https://eu.ui-avatars.com/api/?name=test&background=random',
+      };
+      const res = await (await request(servicio).post('/users').send(newUser));
+      expect(res.statusCode).toEqual(201);
+      // Chequeamos propiedades con valores
+      expect(res.body).toHaveProperty('name', newUser.name);
+      expect(res.body).toHaveProperty('username', newUser.username);
+      userTestID = res.body.id; // Guardamos el ID
+    });
+  });
+  describe('PUT: Tests de actualización de usuarios ', () => {
+    test('PUT: Actualiza un usuario por ID', async () => {
+      const updateUser = {
+        name: 'Usuario Actualización',
+        username: 'usertest',
+        email: 'update@update.test',
+        avatar: 'https://eu.ui-avatars.com/api/?name=test&background=random',
+      };
+      const res = await (await request(servicio).put(`/users/${userTestID}`).send(updateUser));
+      expect(res.statusCode).toEqual(200);
+      // Chequeamos propiedades y valores
+      expect(res.body).toHaveProperty('name', updateUser.name);
+      expect(res.body).toHaveProperty('username', updateUser.username);
+    });
+    test('PUT: NO Actualiza un usuario por ID', async () => {
+      const userID = 'pepe';
+      const updateUser = {
+        name: 'Usuario Actualización',
+        username: 'usertest',
+        email: 'update@update.test',
+        avatar: 'https://eu.ui-avatars.com/api/?name=test&background=random',
+      };
+      const res = await (await request(servicio).put(`/users/${userID}`).send(updateUser));
+      expect(res.statusCode).toEqual(404);
+      // Chequeamos propiedades y valores
     });
   });
 });
